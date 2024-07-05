a368e494af36af3ca3ec97484456e09348a67656
nibroos <nibrosmaverick3@gmail.com>
2024-07-06 01:22:34 +0700

    Initial frontend

---
+++ frontend/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import useLayoutsStore from '~/stores/configs/LayoutsStore';
+
+const layoutStore = useLayoutsStore()
+const { titlePath, subTitlePath, lastPathSegment, parentTitle, topTitle } =
+  storeToRefs(layoutStore)
+
+const isDark = usePreferredDark()
+</script>
+
+<template>
+  <div>
+    Welkam Nibros
+  </div>
+</template>
